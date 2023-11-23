f02c1f98031f81ee19cf19292a8abe775c0a9cd9
Furini2002 <llfurini2002@gmail.com>
2023-11-22 21:05:19 -0300

    atualização da aula de 06/11

--- src/projetovendas/classes/Cliente.java
+++ src/projetovendas/classes/Cliente.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package projetovendas.classes;
 
 import projetovendas.interfaces.IPesquisar;
 
-/**
- *
- * @author aluno
- */
 public class Cliente extends Pessoa {
     
     private Double renda;
--- src/projetovendas/controller/CidadeController.java
+++ src/projetovendas/controller/CidadeController.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package projetovendas.controller;
 
 import projetovendas.interfaces.IOperacao;
 import projetovendas.model.Cidade;
 
-
-/**
- *
- * @author aluno
- */
 public class CidadeController implements IOperacao{
     
     private Cidade cidade;
